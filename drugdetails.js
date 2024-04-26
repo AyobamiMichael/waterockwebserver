fb6c43702014d3b9c21d92f31809264ec10ad1a9
Ayobami <ayobamiwealth@gmail.com>
2024-04-26 18:07:57 +0100

    Update

--- drugdetails.js
+++ drugdetails.js
@@ -53,7 +53,8 @@ const barSchema = new mongoose.Schema({
   barAddress: {type: String, required: true},
   barPhone: {type: String, required: true},
   barManagerUserName:{type: String, required: true},
-  barImage:{type: String, required: false}
+  barImages:[{type: String, required: false}]
+ 
   
 })
 
